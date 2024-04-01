2ba69550000b0bdbce34112fde05fd7a7ef427b4
Nazar <nnakapeliukh@gmail.com>
2024-03-31 19:37:34 -0500

    Adds all changes for the project

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,39 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+  const { budget, dispatch, expenses, currency } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
+  const handleBudgetChange = (event) => {
+    const newValue = event.target.value;
+    const totalExpenses = expenses.reduce((total, item) => {
+      return (total = total + item.cost);
+    }, 0);
+    if (newValue > 20000) {
+      alert(`The new value (${newValue}) cannot exceed 20000`);
+      return;
     }
-    return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
-    );
+    if (newValue < totalExpenses) {
+      alert(`The new value (${newValue}) cannot be lower than spending (${totalExpenses})`);
+      return;
+    }
+    setNewBudget(newValue);
+    dispatch({
+      type: "SET_BUDGET",
+      payload: newValue,
+    });
+  };
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: {currency}</span>
+      <input
+        type="number"
+        step="10"
+        value={newBudget}
+        onChange={handleBudgetChange}
+        width={"100px"}
+      ></input>
+    </div>
+  );
 };
 export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,39 +1,70 @@
-
-import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { TiDelete } from "react-icons/ti";
+import { AppContext } from "../context/AppContext";
+import plusIcon from "./img/plus.png";
+import minusIcon from "./img/minus.png";
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+  const { dispatch, currency } = useContext(AppContext);
 
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: "DELETE_EXPENSE",
+      payload: props.id,
+    });
+  };
+
+  const increaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
     };
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+  };
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
+    };
 
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
+    dispatch({
+      type: "RED_EXPENSE",
+      payload: expense,
+    });
+  };
 
-    }
-
-    return (
-        <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>
+        {currency}
+        {props.cost}
+      </td>
+      <td>
+        <button
+          style={{ backgroundColor: "#00000000", border: "none" }}
+          onClick={(event) => increaseAllocation(props.name)}
+        >
+          <img alt="plus icon" src={plusIcon} width={"25px"} />
+        </button>
+      </td>
+      <td>
+        <button
+          style={{ backgroundColor: "#00000000", border: "none" }}
+          onClick={(event) => decreaseAllocation(props.name)}
+        >
+          <img alt="plus icon" src={minusIcon} width={"25px"} />
+        </button>
+      </td>
+      <td>
+        <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
+      </td>
+    </tr>
+  );
 };
 
 export default ExpenseItem;
